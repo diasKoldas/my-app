f2084e1831b44c861622e421a33d556699ebeb86
diasKoldas <dias.koldas@gmail.com>
2019-11-29 23:24:28 +0600

    Add files via upload

---
+++ autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+require '../db/database.php';
+require '../helper/functions.php';
+require '../app/controllers/MainController.php';
+require '../vendor/diaskoldas/my-route/src/Route.php';
+require '../vendor/diaskoldas/my-route/src/Helper.php';
+require '../vendor/diaskoldas/my-route/src/Parser.php';
+require '../vendor/diaskoldas/my-querybuilder/src/QueryFactory.php';
+require '../vendor/diaskoldas/my-querybuilder/src/QueryBuilder.php';
